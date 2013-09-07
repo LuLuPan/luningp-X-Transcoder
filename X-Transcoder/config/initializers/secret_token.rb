99fbdda354414dc467c8efa268b8eda7001389f1
LuLuPan <luning.pan@hotmail.com>
2013-09-06 22:37:47 -0400

    first commit

---
+++ X-Transcoder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XTranscoder::Application.config.secret_token = 'cb741e2fbdba93d67fee1593b9744d4637e46b722ec26165afe219953ab2f43ad705d7f8f3b10596221f035a582444ad262b628e0243c557731b2f955e7ccbb9'
