504a7715ae9112c42bc470b5147eb44b9660e51a
LuLuPan <luning.pan@hotmail.com>
2013-09-08 17:13:35 -0400

    Begin user auth

--- X-Transcoder/app/models/user.rb
+++ X-Transcoder/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
   has_many :XCoder, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
--- X-Transcoder/db/schema.rb
+++ X-Transcoder/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908060829) do
+ActiveRecord::Schema.define(:version => 20130908203443) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   create_table "x_coders", :force => true do |t|
