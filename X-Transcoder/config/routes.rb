6aa49c82393a64bf7455de44b7ce3f454e5cd13b
LuLuPan <luning.pan@hotmail.com>
2013-09-08 00:17:01 -0400

    User Controller/Module

--- X-Transcoder/config/routes.rb
+++ X-Transcoder/config/routes.rb
@@ -1,6 +1,8 @@
 XTranscoder::Application.routes.draw do
   get "users/new"
 
+  get "users/new"
+
   root :to => 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
