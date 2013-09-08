2ad231c7bcc9c2c2eeb330b913dce12bd46923fb
LuLuPan <luning.pan@hotmail.com>
2013-09-08 15:54:33 -0400

    backup more

--- X-Transcoder/app/controllers/x_coder_controller.rb
+++ X-Transcoder/app/controllers/x_coder_controller.rb
@@ -1,4 +1,22 @@
 class XCoderController < ApplicationController
+  before_action :signed_in_user
   def new
+  	@xcoder = XCoder.new
+  end
+
+  def index
+  end
+
+  def create
+  	@xcoder = current_user.xcoder.build(xcoder_params)
+  	if @xcoder.save
+  		flash[:success] = "Xcoder created!"
+  		redirect_to root_url
+  	else
+  		render 'static_pages/home'
+  	end
+  end
+
+  def destroy
   end
 end
--- X-Transcoder/app/models/x_coder.rb
+++ X-Transcoder/app/models/x_coder.rb
@@ -1,11 +1,12 @@
 class XCoder < ActiveRecord::Base
-  attr_accessible :format, :stream, :user_id
+  attr_accessible :stream, :format_in, :format_out, :user_id
   belongs_to :user
   #default order, FIFO to schedule x-code task
   #default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
   #stream name 
   validates :stream, presence: true
-  #video format name
-  validates :format, presence: true, length: { maximum: 20 }
+  #video i/o format name
+  validates :format_in, presence: true, length: { maximum: 20 }
+  validates :format_out, presence: true, length: { maximum: 20 }
 end
