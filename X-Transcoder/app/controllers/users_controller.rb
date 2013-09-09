7ba4ddf1b90b15246aea467304c21b4d54dd001e
LuLuPan <luning.pan@hotmail.com>
2013-09-08 21:08:25 -0400

    user create

--- X-Transcoder/app/controllers/users_controller.rb
+++ X-Transcoder/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
@@ -15,4 +16,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
